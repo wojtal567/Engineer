96bb6383a60dffe7f4b1cc8bff64e989e5ff3906
wojtal567 <kamilwojtalik98@gmail.com>
2020-06-08 17:31:21 +0200

    Added SD, database and RTC support

--- platformio/src/main.cpp
+++ platformio/src/main.cpp
@@ -1,7 +1,12 @@
 #include <WiFi.h>
+#include <Wire.h>
 #include <SPI.h>
 #include <TFT_eSPI.h>
 #include <SD.h>
+#include <sqlite3.h>
+#include <RtcDS1307.h>
+#include <time.h>
+RtcDS1307<TwoWire> Rtc(Wire);
 //#include <NTPClient.h>
 #include <WEMOS_SHT3X.h> 
 //#include "TFT_eSPI/Fonts/Font16.h"
@@ -13,6 +18,12 @@
 #define menuRectBottom 225
 #define menuRectLeft 14
 #define menuRectRight 305
+
+
+sqlite3 *db = NULL;
+char db_file_name[100] = "\0";
+char *zErrMsg = 0;//jakos to trza wyrzucic
+const char* data1 = "Output:";//tez
 
 struct pms5003data {
   uint16_t framelen;
@@ -27,8 +38,12 @@ TFT_eSPI tft = TFT_eSPI();
 SHT3X sht30(0x45);
 
 int temp, humi;
-const char* ssid = "";
-const char* password = "";
+const char* ssid = "Kamil";
+const char* password = "qetuo13579";
+const char* ntpServer = "0.pool.ntp.org";
+const long gmtOffset_sec = 3600;
+const int daylightOffset_sec = 3600;
+
 
 char *titles[] = { "PM2.5     ug/m3", "TEMP",  "RH",  "PM1.0   ug/m3",  "PM10.0    ug/m3",  "PM10  PARTICLES", "Air quality:"};
 
@@ -59,6 +74,128 @@ uint16_t  RGB16b(uint8_t red, uint8_t green, uint8_t blue){
   return (red * 2048) + (green * 32) + blue;
 }
 
+int db_open() {
+  if (db != NULL)
+    sqlite3_close(db);
+  int rc = sqlite3_open(db_file_name, &db);
+  return rc;
+}
+
+
+void db_createtable()
+{
+  if (db == NULL) {
+    Serial.println("No database open");
+  }
+  String sql = "CREATE table if not exists samples (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, temperature float, humidity FLOAT, pm10 FLOAT, pm25 FLOAT, pm100 float, timestamp datetime NOT NULL default CURRENT_TIMESTAMP)";
+  int rc = sqlite3_exec(db, sql.c_str(), 0, (void*)data1, &zErrMsg);
+  if (rc != SQLITE_OK) {
+    sqlite3_free(zErrMsg);
+  }
+}
+
+boolean start_SD()
+{
+  bool result = SD.begin(27);
+  if(result)
+  {
+    sqlite3_initialize();
+    strncpy(db_file_name,"/sd/database.db", 100);
+    db_open();
+    db_createtable();
+    sqlite3_close(db);
+    SD.end();
+  }
+  return result; 
+}
+
+String timestamp(){
+  RtcDateTime timestamp = Rtc.GetDateTime();
+  String time = (String)timestamp.Year()+"-";
+  if(timestamp.Month()<10)
+  {
+    time+="0"+(String)timestamp.Month()+"-";
+  }else
+  {
+    time+=(String)timestamp.Month()+"-";
+  } 
+  if(timestamp.Day()<10){
+    time+="0"+(String)timestamp.Day()+" ";
+  }else
+  {
+    time+=(String)timestamp.Day()+" ";
+  }
+  if(timestamp.Hour()<10)
+  {
+    time+="0"+(String)timestamp.Hour()+":";
+  }else
+  {
+    time+=(String)timestamp.Hour()+":";
+  }
+  if(timestamp.Minute()<10)
+  {
+    time+="0"+(String)timestamp.Minute()+":";
+  }else
+  {
+    time+=(String)timestamp.Minute()+":";
+  }
+  if(timestamp.Second()<10){
+    time+="0"+(String)timestamp.Second();
+  }else
+  {
+    time+=(String)timestamp.Second();
+  }
+  return time;
+}
+
+int db_save(int temp, int humi) {
+  if (db == NULL) {
+    return 0;
+  }
+  String sql = "insert into samples (temperature, humidity, pm10, pm25, pm100, timestamp) values ("+(String)temp+", "+(String)humi+", "+(String)data.pm10_standard+", "+(String)data.pm25_standard+", "+(String)data.pm100_standard+", "+"'"+timestamp()+"')";
+  int rc = sqlite3_exec(db, sql.c_str(), 0, (void*)data1, &zErrMsg);
+  if (rc != SQLITE_OK) {
+    Serial.print(F("SQL error: "));
+    Serial.print(sqlite3_extended_errcode(db));
+    Serial.print(" ");
+    Serial.println(zErrMsg);
+    sqlite3_free(zErrMsg);
+  }
+  return rc;
+}
+
+void save_card(){
+  if(!SD.begin(27))
+  {
+    tft.setTextSize(1);
+    tft.setCursor(0, 0);
+    tft.fillRect(0, 0, 100, 13, TFT_BLACK);
+    tft.print("No SD Card");
+  }    
+  else
+  {
+    tft.setTextSize(1);
+    tft.setCursor(0, 0);
+    tft.fillRect(0, 0, 100, 13, TFT_BLACK);
+    tft.print("SD Card detected");
+    if(SD.exists("/database.db"))
+    {
+      db_open();
+      db_createtable();
+      db_save(temp, humi);
+      sqlite3_close(db);
+    }
+    else
+    {
+      db_open();
+      db_createtable();
+      db_save(temp, humi);
+      sqlite3_close(db);
+    }    
+    SD.end();
+  }
+}
+
 void tftMenuInit(){
   double col_hex =  0xF800;
   tft.begin();
@@ -70,6 +207,12 @@ void tftMenuInit(){
   tft.drawLine(14, 145, 305, 145, TFT_WHITE);
   tft.drawLine(14, 200, 305, 200, TFT_WHITE);
   tft.drawRect(14, 14, 292, 212, TFT_WHITE);
+  tft.setTextSize(1);
+  tft.setCursor(0, 0);
+  if(!start_SD())
+    tft.print("No SD Card");
+  else
+    tft.print("SD Card detected");
   tft.setTextSize(2);
   for(uint8_t i=0;i<7;i++){
     tft.setCursor(titlesPositionXY[i][0], titlesPositionXY[i][1]);
@@ -163,6 +306,40 @@ boolean readPMSdata(Stream *s) {
   return true;
 }
 
+void printDateTime(const RtcDateTime& dt)
+{
+    char datestring[20];
+ 
+    snprintf_P(datestring, 
+            20,
+            PSTR("%02u/%02u/%04u %02u:%02u:%02u"),
+            dt.Day(),
+            dt.Month(),
+            dt.Year(),
+            dt.Hour(),
+            dt.Minute(),
+            dt.Second() );
+    tft.setTextSize(1);
+    tft.setCursor(200, 0);
+    tft.fillRect(200, 0, 300, 13, TFT_BLACK);
+    tft.print((String) datestring);
+}
+
+void config_time()
+{
+  delay(500);
+  Rtc.Begin();
+  time_t rawtime;
+  struct tm* timeinfo;
+  time(&rawtime);
+  timeinfo = localtime(&rawtime);
+  char year[5];
+  char month[5];
+  strftime (year, sizeof(year), "%Y",timeinfo);
+  strftime (month, sizeof(month), "%m", timeinfo);
+  RtcDateTime date = RtcDateTime(atoi(year), atoi(month), timeinfo->tm_mday, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+  Rtc.SetDateTime(date);
+}
 
 void setup(){
 //  tft.loadFont();
@@ -172,22 +349,47 @@ void setup(){
   tft.init();
   tft.setRotation(1);
   tft.fillScreen(TFT_BLACK);
-  tftMenuInit();
+  tftMenuInit();  
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED and wifiAttempts>0){
     Serial.print(".");
     delay(500);
     wifiAttempts--;
+  } 
+  if(WiFi.status()==WL_CONNECTED)
+  {
+    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+    delay(500);
+    config_time();
+    tft.setTextSize(1);
+    tft.setCursor(0, 230); 
+    Serial.println(WiFi.localIP());
+    tft.print("IP address: "+ WiFi.localIP().toString());
   }
+  else
+  {
+    tft.setTextSize(1);
+    tft.setCursor(0, 230);
+    tft.print("No WiFi connection");
+  }
+   
+  
 }
 
 void loop(){
+  
   sht30.get(); 
   temp = sht30.cTemp;
   humi = sht30.humidity;
   if(readPMSdata(&Serial2)){
     tftClearData();
     tftDisplayData();
+    save_card();   
+    RtcDateTime now = Rtc.GetDateTime();
+    printDateTime(now);   
   }
-  delay(3000);
+
+delay(3000);
 }
+ 
+  
